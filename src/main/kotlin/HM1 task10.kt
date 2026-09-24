a6b57c61544a50cb04f8c55ae8197f031eecdb03
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-24 14:13:00 +0500

    HW1: Task 10

--- src/main/kotlin/HM1 task10.kt
+++ src/main/kotlin/HM1 task10.kt
@@ -1,2 +1,10 @@
-class `HM1 task10` {
+fun main() {
+    weather("Ankara", 27, 31, 82)
+    weather("Tokyo", 32,36,10)
+    weather("Cape Town",59,64,2)
+    weather("Guatemala City",50,55,7)
+}
+
+fun weather(name: String, minTemp: Int, maxTemp: Int, rain: Int) {
+    println("City: $name\nLow temperature: $minTemp, High temperature: $maxTemp\nChance of rain: $rain%\n")
 }
