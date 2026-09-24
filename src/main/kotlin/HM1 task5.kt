fe0f7b90400081a956bbdff01180fd710138a0b9
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-24 13:23:41 +0500

    HW1: Task 5

---
+++ src/main/kotlin/HM1 task5.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    var totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+//exactly the same thing as a previous task i changed all vals to vars and it worked. but i tried also to delede " and $
