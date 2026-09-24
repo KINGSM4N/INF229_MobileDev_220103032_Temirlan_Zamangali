796c11acaa8e1d7e646cfc6a58d39c7ed253a895
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-24 14:05:40 +0500

    HW1: Task 9

--- src/main/kotlin/HM1 task9.kt
+++ src/main/kotlin/HM1 task9.kt
@@ -1,2 +1,9 @@
-class `HM1 task9` {
+fun main() {
+    println(compareTime(300, 250))
+    println(compareTime(300, 300))
+    println(compareTime(200, 220))
 }
+
+fun compareTime(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
+    return timeSpentToday > timeSpentYesterday
+}// its compare 2 functions today's and yesterday's screen times and show is it true or false
