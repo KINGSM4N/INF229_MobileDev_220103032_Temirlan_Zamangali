a07babeba4bc63dc38b67b80e936d9bf18672948
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-24 12:46:27 +0500

    HW1: Task 1

---
+++ src/main/kotlin/HM1.kt
@@ -0,0 +1,5 @@
+fun main(){
+    println("Use the val keyword when the value doesn't change. \nUse the var keyword when the value can change.\n" +
+            "When you define a function, you define the parameters that can be passed to it. \n" +
+            "When you call a function, you pass arguments for the parameters.\n")
+}//so i wrote all code with \n to not entering again and again println
---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,33 @@
+//fun main(args: Array<String>) {
+//    drive()
+//    drive("slow") //⇒ driving slow
+//    drive(speed = "turtle-like") //⇒ driving turtle-like
+//}
+//fun drive(speed: String="fast!"){
+//    println("drive "+speed)
+//}
+
+//fun main(args: Array<String>) {
+//    tempToday("Monday", 23)
+//}
+//
+//
+//
+//fun tempToday(day: String, temp: Int){
+//    println("Today is $day and the temperature is $temp")
+//}
+
+//fun main(){
+//    reformat("Today is a day like no other day", false, '_')
+//
+//}
+//fun reformat(str: String, divideByCamelHumps: Boolean, wordSeparation: Char, normalCase: Boolean=true){
+//    println("String: $str, CamelHumps: $divideByCamelHumps, Separator: $wordSeparation, NormalCase: $normalCase")
+//}
+
+fun main(){
+    double(5.25)
+}
+fun double(x: Double) {
+    println(x*2)
+}
