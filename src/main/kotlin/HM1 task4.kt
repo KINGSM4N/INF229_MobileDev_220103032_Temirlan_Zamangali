e63b0cba653aedc1492e87f0e6ca7cc0ca349c7d
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-24 13:19:19 +0500

    HW1: Task 4

---
+++ src/main/kotlin/HM1 task4.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var numberOfAdults: Int = 20
+    var numberOfKids: Int = 30
+    var total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}// when i run the code it showed 2030. and i added Int to do them a number not string and deleted "
+// and as a previous task changed val to var
