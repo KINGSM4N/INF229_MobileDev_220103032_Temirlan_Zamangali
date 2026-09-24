e1142892c9688f177e119b06d9386ab65ba26813
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-24 13:34:53 +0500

    HW1: Task 6

---
+++ src/main/kotlin/HM1 task6.kt
@@ -0,0 +1,34 @@
+//fun main() {
+//    val firstNumber = 10
+//    val secondNumber = 5
+//    var result = firstNumber + secondNumber
+//    println("$firstNumber + $secondNumber = $result")
+//}
+// arono do i need to do step 2 but ill do it
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    var result = add(firstNumber, secondNumber)
+    var anotherResult = add(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+
+    val subtractResult = subtract(firstNumber, secondNumber)
+    val anotherSubtractResult = subtract(firstNumber, thirdNumber)
+
+    println("$firstNumber - $secondNumber = $subtractResult")
+    println("$firstNumber - $thirdNumber = $anotherSubtractResult")
+}
+
+// Define add() function below this line
+fun add(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber + secondNumber
+}
+
+fun subtract(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber + secondNumber
+}
