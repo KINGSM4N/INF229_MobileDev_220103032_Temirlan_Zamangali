de4bdc0443a01d0e7dffa521d4ee08943a65f508
Temirlan <220103032@stu.sdu.edu.kz>
2026-09-25 22:57:24 +0500

    HW2

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -23,9 +23,103 @@
 //    println("String: $str, CamelHumps: $divideByCamelHumps, Separator: $wordSeparation, NormalCase: $normalCase")
 //}
 
-fun main(){
-    double(5.25)
-}
-fun double(x: Double) {
-    println(x*2)
-}
+//fun main(){
+//    double(5.25)
+//}
+//fun double(x: Double) {
+//    println(x*2)
+//}
+
+//fun main() {
+//    println(1 != 1)
+//}
+
+//fun main() {
+//    val trafficLightColor = "green".uppercase()
+//
+//    if (trafficLightColor == "RED") {
+//        println("Stop!")
+//    }
+//    else if(trafficLightColor == "YELLOW"){
+//        println("Be carefully!")
+//    }
+//    else if(trafficLightColor == "GREEN"){
+//        println("Go!")
+//    }
+//    else{
+//        println("Invalid traffic light color!")
+//    }
+//}
+
+//fun main() {
+//    print("Enter traffic light color: ")
+//    val input = readLine()?.uppercase() ?: ""
+//
+//    when (input) {
+//        "RED" -> println("Stop!")
+//        "YELLOW" -> println("Be carefully!")
+//        "GREEN" -> println("Go!")
+//        else -> println("Invalid traffic light color!")
+//    }
+//}ai generated
+
+
+//fun main(){
+//    print("What's the traffic light color? ")
+//    val input = readLine()?.uppercase()?: ""
+//
+//    when(input){
+//        "RED" -> println("Stop!")
+//        "YELLOW", "AMBER" -> println("Be carefully!")
+//        "GREEN" -> println("GO!")
+//        else -> println("Invalid traffic light color")
+//    }
+//}//me
+
+
+//fun main() {
+//    val x = 3
+//
+//    when (x) {
+//        2,3,5,7 -> println("x is a prime number between 1 and 10.")
+//        else -> println("x isn't a prime number between 1 and 10.")
+//    }
+//}
+
+//fun main(){
+//    print("Enter traffic light color: ")
+//    val input = readLine()?.uppercase()?: ""
+//
+//    val message =
+//    if(input=="RED") "Stop!"
+//    else if(input=="YELLOW") "Be carfully!"
+//    else if(input=="GREEN") "Go!"
+//    else "You entered the wrong color"
+//
+//    println(message)
+//}
+
+//fun main(){
+//    print("Enter traffic light color: ")
+//    val input = readLine()?.lowercase()?: ""
+//
+//    val message = when(input){
+//        "red" -> "Stop!"
+//        "yellow", "ember" -> "Be carefully!"
+//        "green" -> "Go!"
+//        else -> "Invalid traffic light color!"
+//
+//    }
+//    println(message)
+//}
+
+
+//fun main() {
+//    var favoriteActor: String? = "Christian Bale"
+//    favoriteActor = null
+//    println(favoriteActor.length)
+//}
+//
+
+
+
